f1a089e8fe0efa30bcc41f80c16fb560641770dc
Bhaksar <bhaskarmandal369@gmail.com>
2024-01-03 17:54:38 +0530

    initial commit

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/bhaskaraa45/todo_app
+
+go 1.21.5
---
+++ frontend/todo/android/app/src/main/kotlin/com/aa45/todo/todo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aa45.todo.todo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
